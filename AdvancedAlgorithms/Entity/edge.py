fec454f7e8effe6e37efdab4e2960c419fecd01e
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-05 14:46:56 +0330

    more function has been added to vertex and edge class

--- AdvancedAlgorithms/Entity/edge.py
+++ AdvancedAlgorithms/Entity/edge.py
@@ -2,7 +2,25 @@
 
 class Edge:
 
-    def __init__(self, source, destination):
+    def __init__(self, source, destination, is_directed = False, weight = 1):
         self.__source = source
         self.__destination = destination
-        self.__is_directed = False
+        self.__is_directed = is_directed
+        self.__weight = weight
+    
+    @property
+    def source(self):
+        return self.__source
+    
+    @property
+    def destination(self):
+        return self.__destination
+
+    @property
+    def is_directed(self):
+        return self.__is_directed
+    
+    @property
+    def weight(self):
+        return self.__weight
+    
--- AdvancedAlgorithms/Entity/vertex.py
+++ AdvancedAlgorithms/Entity/vertex.py
@@ -5,3 +5,16 @@ class Vertex:
     def __init__(self, id):
         self.__id = id
         self.__edges = list()
+
+    def add_edge(self, edge):
+        self.__edges.append(edge)
+    
+    @property
+    def ID(self):
+        return self.__id
+
+    @property
+    def edges(self):
+        return self.__edges
+    
+    
