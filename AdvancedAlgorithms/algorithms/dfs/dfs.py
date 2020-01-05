e568443223c6cfb65fcb8041bb4b36578144d667
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-05 19:46:28 +0330

    dfs package has been initial

---
+++ AdvancedAlgorithms/algorithms/dfs/dfs.py
@@ -0,0 +1,8 @@
+from AdvancedAlgorithms.usecase.graph import Graph
+from AdvancedAlgorithms.entity.vertex import Vertex
+from AdvancedAlgorithms.entity.edge import Edge
+
+class DFS:
+
+    def run(self, graph):
+        pass
---
+++ AdvancedAlgorithms/tests/algorithms/test_dfs.py
@@ -0,0 +1,25 @@
+import pytest
+from hamcrest import *
+
+from AdvancedAlgorithms.algorithms.dfs.dfs import DFS
+from AdvancedAlgorithms.usecase.graph import Graph
+from AdvancedAlgorithms.entity.vertex import Vertex
+from AdvancedAlgorithms.entity.edge import Edge
+
+class TestDFS:
+
+    def test_GivenAGraphToAlgorithmThenCheckTheOutput(self):
+        graph = Graph()
+        matric = [[0, 1, 1, 0, 0, 0]
+                 ,[1, 0, 0, 1, 1, 0]
+                 ,[1, 0, 0, 0, 0, 1]
+                 ,[0, 1, 0, 0, 0, 0]
+                 ,[0, 1, 0, 0, 0, 0]
+                 ,[0, 0, 1, 0, 0, 0]]
+        graph.create(matric, 6, False)
+        dfs = DFS()
+        output_vertices = dfs.run(graph)
+        vertices_id = list()
+        for vertex in output_vertices:
+            vertices_id.append(vertex.ID)
+        assert_that(vertices_id, equal_to([0, 1, 3, 4, 2, 5]))
