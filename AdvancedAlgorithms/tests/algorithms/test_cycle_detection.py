1ccd18384fc52b23090de3c967a1b0112b2cb890
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-06 23:57:47 +0330

    more test has been added to directed cycle detection algorithm

--- AdvancedAlgorithms/tests/algorithms/test_cycle_detection.py
+++ AdvancedAlgorithms/tests/algorithms/test_cycle_detection.py
@@ -13,10 +13,21 @@ class TestCycleDetection:
     def test_GivenAGraphWithCycleThenAlgorithmShouldReturnTrue(self):
         graph = Graph()
         matric = [[0, 1, 0, 0]
-                 ,[0, 0, 1, 1]
-                 ,[1, 0, 0, 1]
-                 ,[0, 0, 0, 0]]
+                 ,[0, 0, 1, 0]
+                 ,[0, 0, 0, 1]
+                 ,[0, 1, 0, 0]]
         graph.create(matric, 4, True)
         cycle_detection = DirectedCycleDetection()
         is_cycle = cycle_detection.run(graph)
         assert_that(is_cycle, equal_to(True))
+    
+    def test_GivenAGraphWithNoCycleThenAlgorithmShouldReturnFalse(self):
+        graph = Graph()
+        matric = [[0, 1, 0, 0]
+                 ,[0, 0, 1, 0]
+                 ,[0, 0, 0, 1]
+                 ,[0, 0, 0, 0]]
+        graph.create(matric, 4, True)
+        cycle_detection = DirectedCycleDetection()
+        is_cycle = cycle_detection.run(graph)
+        assert_that(is_cycle, equal_to(False))
