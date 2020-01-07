84f5b1d6466688f3bf98b5389e6bbec35e788e06
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-07 18:22:24 +0330

    kruskal has been implemented

--- AdvancedAlgorithms/algorithms/spanning_trees/kruskal.py
+++ AdvancedAlgorithms/algorithms/spanning_trees/kruskal.py
@@ -6,4 +6,37 @@ from AdvancedAlgorithms.usecase.graph import Graph
 class Kruskal:
 
     def run(self, graph):
-        pass
+        edges = graph.edges
+        vertices = graph.vertices
+        edges = self.__sort_edges(edges)
+        tree_edges = list()
+        tree_vertices = list()
+        for edge in edges:
+            if len(vertices) == 0:
+                break;
+            tree_edges.append(edge)
+            try:
+                tree_vertices.index(edge.source)
+            except ValueError:
+                tree_vertices.append(edge.source)
+                vertices.remove(edge.source)
+            try:
+                tree_vertices.index(edge.destination)
+            except ValueError:
+                tree_vertices.append(edge.destination)
+                vertices.remove(edge.source)
+        tree_graph = Graph()
+        tree_graph.vertices = tree_vertices.copy()
+        tree_graph.edges = tree_edges.copy()
+        return tree_graph
+        
+    
+    def __sort_edges(self, edges):
+        sort_list = list()
+        for edge in edges:
+            index = 0
+            for e in sort_list:
+                if (edge.weight < e.weight):
+                    index = index + 1
+            sort_list.insert(index, sort_list)
+        return sort_list
