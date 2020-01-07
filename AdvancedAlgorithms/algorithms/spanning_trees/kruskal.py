63c53d19887282d8145fb340b50a1a7a368ac3a4
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-07 18:05:18 +0330

    krusal package has been initiated

---
+++ AdvancedAlgorithms/algorithms/spanning_trees/kruskal.py
@@ -0,0 +1,9 @@
+from AdvancedAlgorithms.entity.vertex import Vertex
+from AdvancedAlgorithms.entity.edge import Edge
+from AdvancedAlgorithms.usecase.graph import Graph
+
+
+class Kruskal:
+
+    def run(self, graph):
+        pass
---
+++ AdvancedAlgorithms/tests/algorithms/test_kruskal.py
@@ -0,0 +1,31 @@
+import pytest
+from hamcrest import *
+
+from AdvancedAlgorithms.algorithms.spanning_trees.kruskal import Kruskal
+from AdvancedAlgorithms.usecase.graph import Graph
+from AdvancedAlgorithms.entity.vertex import Vertex
+from AdvancedAlgorithms.entity.edge import Edge
+
+class TestKruskal:
+    def setup_method(self, method):
+        pass
+
+    def test_GiveAGraphThenAlgorithmShouldReturnTheMinimumSpanningTree(self):
+        graph = Graph()
+        matric = [[0,   2,  6,  0,  5,  10, 0]
+                 ,[2,   0,  0,  3,  3,  0,  0]
+                 ,[6,   0,  0,  1,  0,  2,  0]
+                 ,[0,   3,  1,  0,  4,  0,  5]
+                 ,[5,   3,  0,  4,  0,  0,  0]
+                 ,[10,  0,  2,  0,  0,  0,  5]
+                 ,[0,   0,  0,  5,  0,  5,  0]]
+        graph.create(matric, 7, False)
+        kruskal = Kruskal()
+        tree = kruskal.run(graph)
+        assert_that(tree, equal_to([[0, 2, 0, 0, 0, 0, 0]
+                                   ,[2, 0, 0, 3, 3, 0, 0]
+                                   ,[0, 0, 0, 1, 0, 2, 0]
+                                   ,[0, 3, 1, 0, 0, 0, 0]
+                                   ,[0, 3, 0, 0, 0, 0, 0]
+                                   ,[0, 0, 2, 0, 0, 0, 5]
+                                   ,[0, 0, 0, 0, 0, 5, 0]]))
